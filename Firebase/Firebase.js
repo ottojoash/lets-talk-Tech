3c62cff022704d63c6f30aa1ae6ce66e4b0c82a9
ottojoash <louisjoshbricks@gmail.com>
2023-03-13 18:57:47 +0300

    firebase

--- Firebase/Firebase.js
+++ Firebase/Firebase.js
@@ -4,13 +4,20 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBdVoUs55oDR0IcgjqhNiDQkgY-sMHEJdo",
+
+  authDomain: "lets-talk-e0d18.firebaseapp.com",
+
+  projectId: "lets-talk-e0d18",
+
+  storageBucket: "lets-talk-e0d18.appspot.com",
+
+  messagingSenderId: "296956508711",
+
+  appId: "1:296956508711:web:d473dd342ad5a8153e6c02",
+
+  measurementId: "G-Y26PNTWPCD"
+
 };
 const app = initializeApp(firebaseConfig);
 
